674ea1502b29c6fc370cf01693e2764dfc3e3521
stalat <talatparwez27@gmail.com>
2024-07-23 02:16:51 +0530

    directory issue fixed

--- envsetup.sh
+++ envsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [-d "env"]
+if [ -d "env" ]
 then
   echo "Python virtual env exists"
 else
@@ -12,7 +12,7 @@ source env/bin/activate
 
 pip3 install -r requirements.txt
 
-if [-d "logs"]
+if [ -d "logs" ]
 then
   echo "Log folder exists"
 else
